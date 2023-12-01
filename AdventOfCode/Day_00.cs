e4381162859e4b293260c70429dd2e4b79bf9ce9
Magic fTail <runecnielsen3132@gmail.com>
2023-12-01 14:23:16 +0100

    Download template and setup

---
+++ AdventOfCode/Day_00.cs
@@ -0,0 +1,21 @@
+﻿namespace AdventOfCode;
+
+public class Day_00 : BaseDay
+{
+    private readonly string _input;
+
+    public Day_00()
+    {
+        _input = File.ReadAllText(InputFilePath);
+    }
+
+    public override ValueTask<string> Solve_1()
+    {
+        return new("");
+    }
+
+    public override ValueTask<string> Solve_2()
+    {
+        return new("");
+    }
+}
